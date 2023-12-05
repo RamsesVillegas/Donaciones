81ada3c8f527a4f103f0333345b7cac91d5fafe7
Jona-HN <herrera.jonathan@uabc.edu.mx>
2023-12-05 15:48:45 -0800

    Redireccionamiento a las vistas correspondientes según el tipo de usuario

--- controllers/sistema.js
+++ controllers/sistema.js
@@ -1,3 +1,5 @@
+
+
 const index = (req, res) => {
   res.render('index');
 };
@@ -14,7 +16,25 @@ const registro = (req, res) => {
   res.render('registro');
 };
 
+const redireccionar = (req, res) => {
+  switch(req.user.rol) {
+    // Donadores
+    case 1:
+      res.redirect('/donar');
+      break;
+    // Coordinadores
+    case 2:
+      res.redirect('/coordi');
+      break;
+    // Administradores
+    case 3:
+      res.redirect('/admin');
+      break;
+  }
+}
+
 module.exports.index = index;
 module.exports.donar = donar;
 module.exports.login = login;
 module.exports.registro = registro;
+module.exports.redireccionar = redireccionar;
